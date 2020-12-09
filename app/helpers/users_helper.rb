9c29549834395058fc9e2a7f51d361831850602b
che <che.blancoo58@gmail.com>
2020-12-09 22:31:51 +0100

    logic removed from view and put in helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def show_errors(_user)
+    if @user.errors.any?
+      puts 'error_explanation'
+      puts pluralize(@user.errors.count, 'error')
+      print prohibited this post from being saved:
+      @user.errors.full_messages.each do |msg|
+        puts msg
+      end
+    end
+  end
 end
