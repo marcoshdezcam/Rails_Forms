d3f7d8124b64a74eab3569be366ba6e4070bdaf4
che <che.blancoo58@gmail.com>
2020-12-09 20:39:35 +0100

    Implement some changes as requested by the TSE

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
